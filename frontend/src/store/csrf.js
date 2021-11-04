7bb9147251c6bca4494fa0d3e3b54b051e977656
Sean Pan <seanhpan@gmail.com>
2021-11-04 13:26:17 -0700

    phase 0 done

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -1,7 +1,9 @@
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
 export async function csrfFetch(url, options = {}) {
-    options.method = optiond.method || "GET";
+    // set options.method to 'GET' if there is no method
+    options.method = options.method || 'GET';
+    // set options.headers to an empty object if there is no headers
     options.headers = options.headers || {};
 
     // if the options.method is not 'GET', then set the "Content-Type" header to
@@ -22,4 +24,9 @@ export async function csrfFetch(url, options = {}) {
     // if the response status code is under 400, then return the response to the
     // next promise chain
     return res;
+}
+
+// call this to get the "XSRF-TOKEN" cookie, should only be used in development
+export function restoreCSRF() {
+    return csrfFetch('/api/csrf/restore');
 }
