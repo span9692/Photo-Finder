00ed7b562af8d0cad26193973eaa31d6b33b7fd0
Sean Pan <seanhpan@gmail.com>
2021-11-05 14:56:43 -0700

    modals added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -14,20 +12,18 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return (
-    <>
+  return isLoaded &&(
+    <div>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
-    </>
+      <div className='wrapper'>
+        <div className='sidebar'>
+        <div className='mainbar'>
+        <div className='sidebar'>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,35 +1,37 @@
+// frontend/src/components/Navigation/index.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
 
-const Navigation = ({ isLoaded }) => {
-    const sessionUser = useSelector(state => state.session.user)
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <LoginFormModal />
+        <SignupFormModal />
+      </>
+    );
+  }
 
-    let sessionLinks
-    if (sessionUser) {
-        sessionLinks = (
-            <ProfileButton user={sessionUser} />
-        )
-    } else {
-        sessionLinks = (
-            <>
-                <NavLink to='/login'>Log In</NavLink>
-                <NavLink to='/signup'>Sign Up</NavLink>
-            </>
-        );
-    }
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
+  );
+}
 
-    return (
-        <ul>
-          <li>
-            <NavLink exact to="/">Home</NavLink>
-            {isLoaded && sessionLinks}
-          </li>
-        </ul>
-      );
-    }
-
-    export default Navigation;
+export default Navigation;
