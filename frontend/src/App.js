ff7497691ef09348a88c61f8ab85626977e99ee6
Sean Pan <seanhpan@gmail.com>
2021-11-06 22:35:10 -0700

    splash page nearly done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,33 +8,57 @@ import Navigation from "./components/Navigation";
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [quote, setQuote] = useState(['"We are making photographs to understand what our lives mean to us."', '-Ralph Hattersley'])
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  let lines = [['"Photography is the story I fail to put into words."', "-Destin Sparks"],
+  ['"There is one thing the photo must contain - the humanity of the moment."', '-Robert Frank'],
+  ['"A good snapshot keeps a moment that\'s gone from running away."', '-Eudora Welty'],
+  ['"Photography is the art of making memories tangible."', '-Destin Sparks'],
+  ['"It\'s one thing to make a picture of what a person looks like, it\'s another thing to make a portrait of who they are."', '-Paul Caponigro'],
+  ['"In the world of photography, you get to share a captured moment with other people."', '-James Wilson'],
+  ['"Taking an image, freezing a moment, reveals how rich reality truly is."', '-Anonymous'],
+  ['"The best thing about a picture is that it never changes, even when the people in it do."', '-Andy Warhol'],
+  ['"We are making photographs to understand what our lives mean to us."', '-Ralph Hattersley']
+  ];
+
+  let index = 0;
+
+  useEffect(()=> {
+    let interval = setInterval(() => {
+      setQuote(lines[index])
+      index++;
+      if (index === lines.length) {
+        index = 0;
+      };
+    }, 6000)
+    return () => {clearInterval(interval)}
+  }, [])
 
 
 
-return isLoaded && (
-  <div>
-    <Navigation isLoaded={isLoaded} />
-    <div class='wrapper'>
-      <div className='splash'>
-        <div class='tagline'>
-          <span class='caption1 style1'>Capture The Perfect</span>
-          <span class='caption2 style1'>Moment With Us!</span>
-          <div class='get-started'>
-            <button class='start-button'>Get Started</button>
-          </div>
-          <div class='quote'>
-            <div id='quote1'>"We are making photographs to understand what our lives mean to us."</div>
-            <div id='quote2'>-Ralph Hattersley</div>
+  return isLoaded && (
+    <div>
+      <Navigation isLoaded={isLoaded} />
+      <div class='wrapper'>
+        <div className='splash'>
+          <div class='tagline'>
+            <span class='caption1 style1'>Capture The Perfect</span>
+            <span class='caption2 style1'>Moment With Us!</span>
+            <div class='get-started'>
+              <button class='start-button'>Get Started</button>
+            </div>
+            <div class='quote'>
+              <div id='quote1'>{quote[0]}</div>
+              <div id='quote2'>{quote[1]}</div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
