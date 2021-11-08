45a8c9be136b5f5db1d7688902e4cf793c0f3bd2
Sean Pan <seanhpan@gmail.com>
2021-11-08 01:07:53 -0800

    photographers loaded

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,37 +1,34 @@
 import { useEffect } from 'react'
-// import { showPicture } from '../../store/photos'
+
 import { useSelector, useDispatch } from 'react-redux'
 import { getPhotographer } from '../../store/photographer'
 
 
 function Home() {
     const dispatch = useDispatch();
-    // const photos = useSelector(state => state.photo)
+
     const photographer = useSelector(state => state.photographer)
-
-    // console.log('photographers:', photographer)
-
     let profiles = Object.entries(photographer)
-    console.log(profiles)
 
     useEffect(() => {
-        // dispatch(showPicture())
-        dispatch(getPhotographer())
+         dispatch(getPhotographer())
     }, [dispatch])
-    // let photoUrl
-    // if (photos[1]) {
-    //     photoUrl = <img src={photos[1].url}></img>;
 
-    // } else {
-    //     photoUrl = null;
-
-    // }
 
 return (
     <div class='portraits'>
         {profiles.map(profile => (
         <div>
             <img class='photographer-portraits' src={profile[1].profilePic}></img>
+            <div class='info'>
+                <div>
+                    <div>{profile[1].firstName} {profile[1].lastName}</div>
+                    <div>{profile[1].city}, {profile[1].state}</div>
+                </div>
+                <div>
+                    {'$'}{profile[1].price}/hr
+                </div>
+            </div>
         </div>
         ))}
     </div>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 // frontend/src/components/Navigation/index.js
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
@@ -30,7 +30,7 @@ function Navigation({ isLoaded }) {
         <li>
           <div class='homebar'>
             <div >
-              <NavLink class='home-button' exact to="/">Home</NavLink>
+              <Link class='home-button' exact to="/">Home</Link>
             </div>
             <div>
               {isLoaded && sessionLinks}
