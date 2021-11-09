0fb54d0bca9ea5ba8ec8a0ea15473ffc0e128831
Sean Pan <seanhpan@gmail.com>
2021-11-09 12:06:11 -0800

    remove console log

---
+++ frontend/src/store/review.js
@@ -0,0 +1,15 @@
+import { csrfFetch } from './csrf';
+
+//action creator
+
+//thunk functions
+
+//reducer
+const reviewReducer = (state = {}, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reviewReducer;
