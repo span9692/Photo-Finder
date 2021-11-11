1a83ac99ba33f88f23baaa6bfd0ac7c95c718081
Sean Pan <seanhpan@gmail.com>
2021-11-11 00:45:14 -0800

    seeder file updated

--- backend/db/seeders/20211106091503-reviews.js
+++ backend/db/seeders/20211106091503-reviews.js
@@ -9,7 +9,13 @@ module.exports = {
       Example:
       */
     return queryInterface.bulkInsert('Reviews', [
-      {userId: 1, photographerId: 5, review:'Emily was an amazing photographer! 10/10', createdAt: new Date(), updatedAt:new Date()}
+      {userId: 1, photographerId: 2, review:'very professional', createdAt: new Date(), updatedAt:new Date()},
+      {userId: 2, photographerId: 1, review:'Nick was an amazing photographer! 10/10', createdAt: new Date(), updatedAt:new Date()},
+      {userId: 3, photographerId: 1, review:'Wow! Amazing!', createdAt: new Date(), updatedAt:new Date()},
+      {userId: 4, photographerId: 1, review:'yo dudes lit', createdAt: new Date(), updatedAt:new Date()},
+      {userId: 5, photographerId: 1, review:'Thank you!', createdAt: new Date(), updatedAt:new Date()},
+      {userId: 6, photographerId: 2, review:'love the photos..recommend!', createdAt: new Date(), updatedAt:new Date()},
+      {userId: 7, photographerId: 2, review:'Thank you! 10/10', createdAt: new Date(), updatedAt:new Date()},
     ], {});
   },
 
