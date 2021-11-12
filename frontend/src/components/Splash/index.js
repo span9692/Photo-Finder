201a2dd1793b826c1fbd680a5ed63935a9840ca1
Sean Pan <seanhpan@gmail.com>
2021-11-11 16:13:51 -0800

    github linkedin added to splash page

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -44,6 +44,14 @@ function Splash() {
                             <div id='quote1'>{quote[0]}</div>
                             <div id='quote2'>{quote[1]}</div>
                         </div>
+                        <div class='seanicon'>
+                            <a className='linkedinposition' href="https://github.com/span9692" target="_blank">
+                                <img className='github' src="https://res.cloudinary.com/photofinder/image/upload/v1636674658/GitHub-Mark-120px-plus_txoi2x.png" />
+                            </a>
+                            <a className='linkedinposition' href="https://www.linkedin.com/in/sean-pan-395a4593/" target="_blank">
+                                <img className='linkedin' src="https://res.cloudinary.com/photofinder/image/upload/v1636675007/linkedin-logo-png-2025_p0gjab.png" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
