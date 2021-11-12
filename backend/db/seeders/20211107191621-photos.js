6e4051ef85620380791fceb47772f6ba84ab8469
Sean Pan <seanhpan@gmail.com>
2021-11-11 21:02:50 -0800

    photo seeder

--- backend/db/seeders/20211107191621-photos.js
+++ backend/db/seeders/20211107191621-photos.js
@@ -9,7 +9,16 @@ module.exports = {
       Example:
       */
    return queryInterface.bulkInsert('Photos', [
-     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636312554/wedding1_x42tzm.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()}
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636692234/AllPhoto%20PhotoFinder/wedding%20photos/wedding1_b20x1b.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693119/AllPhoto%20PhotoFinder/wedding%20photos/main-1-2_mcbdpc.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693120/AllPhoto%20PhotoFinder/wedding%20photos/Bride-and-Groom-Hutton-House-Wedding-Minneapolis-MN_qmgyeb.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693120/AllPhoto%20PhotoFinder/wedding%20photos/maxresdefault_kcycat.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693120/AllPhoto%20PhotoFinder/wedding%20photos/01-the-grand-long-beach-wedding-photos-natalie-luca_o1rvja.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693119/AllPhoto%20PhotoFinder/wedding%20photos/44245513_10155923876911984_870515993166741504_n_ipwac3.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693120/AllPhoto%20PhotoFinder/wedding%20photos/home-page-01_mi2q84.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693121/AllPhoto%20PhotoFinder/wedding%20photos/JRRF-38-scaled_boj2e3.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693161/AllPhoto%20PhotoFinder/wedding%20photos/1483735796-1483552460-wedding-photos-30_obvlyi.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
+     {portfolioId: 1, url: 'https://res.cloudinary.com/photofinder/image/upload/v1636693161/AllPhoto%20PhotoFinder/wedding%20photos/summit-ceremony_ieksdv.jpg', type: 'wedding', createdAt: new Date(), updatedAt:new Date()},
     ], {});
   },
 
